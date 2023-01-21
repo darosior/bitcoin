4fd07dab45c199b0d3541081fa1dd7b964451ab9
Antoine Poinsot <darosior@protonmail.com>
2023-01-21 14:37:49 +0100

    descriptor: parse Miniscript expressions within Taproot descriptors

--- src/script/descriptor.cpp
+++ src/script/descriptor.cpp
@@ -1021,23 +1021,29 @@ class MiniscriptDescriptor final : public DescriptorImpl
 {
 private:
     miniscript::NodeRef<uint32_t> m_node;
+    miniscript::MiniscriptContext script_ctx;
 
 protected:
     std::vector<CScript> MakeScripts(const std::vector<CPubKey>& keys, Span<const CScript> scripts,
                                      FlatSigningProvider& provider) const override
     {
-        for (const auto& key : keys) provider.pubkeys.emplace(key.GetID(), key);
-        return Vector(m_node->ToScript(ScriptMaker(keys, miniscript::MiniscriptContext::P2WSH)));
+        for (const auto& key : keys) {
+            if (script_ctx == miniscript::MiniscriptContext::TAPSCRIPT) {
+                provider.pubkeys.emplace(CKeyID(Hash160(XOnlyPubKey{key})), key);
+            }
+            provider.pubkeys.emplace(key.GetID(), key);
+        }
+        return Vector(m_node->ToScript(ScriptMaker(keys, script_ctx)));
     }
 
 public:
-    MiniscriptDescriptor(std::vector<std::unique_ptr<PubkeyProvider>> providers, miniscript::NodeRef<uint32_t> node)
-        : DescriptorImpl(std::move(providers), "?"), m_node(std::move(node)) {}
+    MiniscriptDescriptor(std::vector<std::unique_ptr<PubkeyProvider>> providers, miniscript::NodeRef<uint32_t> node,
+                         miniscript::MiniscriptContext ctx) : DescriptorImpl(std::move(providers), "?"), m_node(std::move(node)), script_ctx(ctx) {}
 
     bool ToStringHelper(const SigningProvider* arg, std::string& out, const StringType type,
                         const DescriptorCache* cache = nullptr) const override
     {
-        if (const auto res = m_node->ToString(StringMaker(arg, m_pubkey_args, type == StringType::PRIVATE, miniscript::MiniscriptContext::P2WSH))) {
+        if (const auto res = m_node->ToString(StringMaker(arg, m_pubkey_args, type == StringType::PRIVATE, script_ctx))) {
             out = *res;
             return true;
         }
@@ -1338,8 +1344,8 @@ std::unique_ptr<DescriptorImpl> ParseScript(uint32_t& key_exp_index, Span<const 
         }
         ++key_exp_index;
         return std::make_unique<PKHDescriptor>(std::move(pubkey));
-    } else if (Func("pkh", expr)) {
-        error = "Can only have pkh at top level, in sh(), or in wsh()";
+    } else if (ctx != ParseScriptContext::P2TR && Func("pkh", expr)) {
+        error = "Can only have pkh at top level, in sh(), wsh(), or in tr()";
         return nullptr;
     }
     if (ctx == ParseScriptContext::TOP && Func("combo", expr)) {
@@ -1552,11 +1558,12 @@ std::unique_ptr<DescriptorImpl> ParseScript(uint32_t& key_exp_index, Span<const 
     }
     // Process miniscript expressions.
     {
-        KeyParser parser_ctx(/*out = */&out, /* in = */nullptr, /* ctx = */miniscript::MiniscriptContext::P2WSH);
+        const auto script_ctx{ctx == ParseScriptContext::P2WSH ? miniscript::MiniscriptContext::P2WSH : miniscript::MiniscriptContext::TAPSCRIPT};
+        KeyParser parser_ctx(/*out = */&out, /* in = */nullptr, /* ctx = */script_ctx);
         auto node = miniscript::FromString(std::string(expr.begin(), expr.end()), parser_ctx);
         if (node) {
-            if (ctx != ParseScriptContext::P2WSH) {
-                error = "Miniscript expressions can only be used in wsh";
+            if (ctx != ParseScriptContext::P2WSH && ctx != ParseScriptContext::P2TR) {
+                error = "Miniscript expressions can only be used in wsh or tr.";
                 return nullptr;
             }
             if (parser_ctx.m_key_parsing_error != "") {
@@ -1586,7 +1593,7 @@ std::unique_ptr<DescriptorImpl> ParseScript(uint32_t& key_exp_index, Span<const 
                 }
                 return nullptr;
             }
-            return std::make_unique<MiniscriptDescriptor>(std::move(parser_ctx.m_keys), std::move(node));
+            return std::make_unique<MiniscriptDescriptor>(std::move(parser_ctx.m_keys), std::move(node), script_ctx);
         }
     }
     if (ctx == ParseScriptContext::P2SH) {
@@ -1719,11 +1726,12 @@ std::unique_ptr<DescriptorImpl> InferScript(const CScript& script, ParseScriptCo
         }
     }
 
-    if (ctx == ParseScriptContext::P2WSH) {
-        KeyParser parser_ctx(/* out = */nullptr, /* in = */&provider, /* ctx = */miniscript::MiniscriptContext::P2WSH);
+    if (ctx == ParseScriptContext::P2WSH || ctx == ParseScriptContext::P2TR) {
+        const auto script_ctx{ctx == ParseScriptContext::P2WSH ? miniscript::MiniscriptContext::P2WSH : miniscript::MiniscriptContext::TAPSCRIPT};
+        KeyParser parser_ctx(/* out = */nullptr, /* in = */&provider, /* ctx = */script_ctx);
         auto node = miniscript::FromScript(script, parser_ctx);
         if (node && node->IsSane(parser_ctx)) {
-            return std::make_unique<MiniscriptDescriptor>(std::move(parser_ctx.m_keys), std::move(node));
+            return std::make_unique<MiniscriptDescriptor>(std::move(parser_ctx.m_keys), std::move(node), script_ctx);
         }
     }
 
