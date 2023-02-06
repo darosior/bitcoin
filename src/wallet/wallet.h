f86e70700e8f8ae48a5313aed626bfdc9225b46b
Andrew Chow <github@achow101.com>
2023-02-06 15:38:53 -0500

    wallet: Introduce a callback called after TopUp completes
    After TopUp completes, the wallet containing each SPKM will want to know
    what new scriptPubKeys were generated. In order for all TopUp calls
    (including ones internal the the SPKM), we use a callback function given
    at the time of the SPKM's construction.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -950,6 +950,9 @@ public:
 
     //! Whether the (external) signer performs R-value signature grinding
     bool CanGrindR() const;
+
+    //! Add scriptPubKeys for this ScriptPubKeyMan into the scriptPubKey cache
+    void CacheNewScriptPubKeys(const std::set<CScript>& spks, ScriptPubKeyMan* spkm);
 };
 
 /**
