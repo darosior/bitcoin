8b840b6e58fcec8890f9060029f92e4002a3fe7b
Antoine Poinsot <darosior@protonmail.com>
2025-10-14 16:20:07 -0400

    validation: make BIP54 sigops check consensus-critical
    When BIP54 is active, make sure transaction in blocks do not violate the BIP54 limit on the
    number of potentially-executed legacy sigops.

--- src/test/fuzz/coins_view.cpp
+++ src/test/fuzz/coins_view.cpp
@@ -255,7 +255,8 @@ FUZZ_TARGET(coins_view, .init = initialize_coins_view)
                     // It is not allowed to call CheckTxInputs if CheckTransaction failed
                     return;
                 }
-                if (Consensus::CheckTxInputs(transaction, state, coins_view_cache, fuzzed_data_provider.ConsumeIntegralInRange<int>(0, std::numeric_limits<int>::max()), tx_fee_out)) {
+                const bool enforce_bip54{!transaction.IsCoinBase() && fuzzed_data_provider.ConsumeBool()};
+                if (Consensus::CheckTxInputs(transaction, state, coins_view_cache, fuzzed_data_provider.ConsumeIntegralInRange<int>(0, std::numeric_limits<int>::max()), tx_fee_out, enforce_bip54)) {
                     assert(MoneyRange(tx_fee_out));
                 }
             },
